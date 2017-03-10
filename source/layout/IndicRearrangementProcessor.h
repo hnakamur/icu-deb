cd408218b2ea4264f66e7529b226e35fa1fbbfe4
Marc Deslauriers <marc.deslauriers@ubuntu.com>
2017-03-10 11:41:10 -0500

    backport of CVE-2015-4844 patch from JDK
    Gbp-Pq: CVE-2015-4844.patch.

--- source/layout/IndicRearrangementProcessor.h
+++ source/layout/IndicRearrangementProcessor.h
@@ -31,7 +31,7 @@ public:
 
     virtual void endStateTable();
 
-    void doRearrangementAction(LEGlyphStorage &glyphStorage, IndicRearrangementVerb verb) const;
+    void doRearrangementAction(LEGlyphStorage &glyphStorage, IndicRearrangementVerb verb, LEErrorCode &success) const;
 
     IndicRearrangementProcessor(const LEReferenceTo<MorphSubtableHeader> &morphSubtableHeader, LEErrorCode &success);
     virtual ~IndicRearrangementProcessor();
@@ -51,8 +51,8 @@ public:
     static UClassID getStaticClassID();
 
 protected:
-    le_int32 firstGlyph;
-    le_int32 lastGlyph;
+    le_uint32 firstGlyph;
+    le_uint32 lastGlyph;
 
     LEReferenceTo<IndicRearrangementSubtableHeader> indicRearrangementSubtableHeader;
     LEReferenceToArrayOf<IndicRearrangementStateEntry> entryTable;
